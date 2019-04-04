c24efc59c259fd0363363455b2646321578cd733
Aronyo Mitra <aronyomitra@gmail.com>
2019-04-05 04:05:10 +0530

    Added a dynamic form

---
+++ js/form.js
@@ -0,0 +1,30 @@
+function addField()
+{
+  let itemCount = document.getElementsByClassName('item').length;
+
+  let newfield = document.createElement("input");
+  newfield.type = 'text';
+  newfield.className = 'item';
+  itemCount++;
+  newfield.name = 'i' + itemCount;
+
+  let br = document.createElement("br");
+
+  let addBtn = document.getElementById('addBtn');
+  document.getElementById('itemform').removeChild(addBtn);
+  document.getElementById('itemform').appendChild(newfield);
+  document.getElementById('itemform').appendChild(addBtn);
+  document.getElementById('itemform').appendChild(br);
+}
+
+function readFields()
+{
+  let items = document.getElementsByClassName('item');
+  values = [];
+
+  for (let i = 0; i < items.length; i++)
+  {
+    values[i] = items[i].value
+  }
+  console.log(values);
+}
