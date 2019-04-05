0a4cda32c6d678b60c7509172eed56891e19fe6d
Aronyo Mitra <aronyomitra@gmail.com>
2019-04-05 14:17:58 +0530

    Add feature to remove fields in form

--- js/form.js
+++ js/form.js
@@ -2,16 +2,18 @@ function addField()
 {
   let itemCount = document.getElementsByClassName('item').length;
 
-  let newfield = document.createElement("input");
-  newfield.type = 'text';
-  newfield.className = 'item';
+  let newfield = document.getElementById('i1').cloneNode(true);
   itemCount++;
-  newfield.name = 'i' + itemCount;
+  newfield.id = 'i' + itemCount;
+
+  let db = document.getElementById('db1').cloneNode(true);
+  db.id = 'db' + itemCount;
 
   let br = document.createElement("br");
 
   let addBtn = document.getElementById('addBtn');
   document.getElementById('itemform').appendChild(newfield);
+  document.getElementById('itemform').appendChild(db);
   document.getElementById('itemform').appendChild(br);
   document.getElementById('itemform').appendChild(addBtn);
 }
@@ -26,4 +28,20 @@ function readFields()
     values[i] = items[i].value
   }
   console.log(values);
+}
+
+function delField(el)
+{
+  let itemCount = document.getElementsByClassName('item').length;
+
+  let id = el.id;
+  let tid = 'i' + id.slice(2,);
+  let tinput = document.getElementById(tid);
+  let br = document.getElementById(id).nextSibling;
+  if (itemCount > 2 && id != 'db1')
+  {
+    document.getElementById('itemform').removeChild(el);
+    document.getElementById('itemform').removeChild(tinput);
+    document.getElementById('itemform').removeChild(br);
+  }
 }
