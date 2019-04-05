c2c018ec7b85c381c949dd915c00cf403d1b3fd6
Aronyo Mitra <aronyomitra@gmail.com>
2019-04-05 16:36:01 +0530

    Fix issue with whitespace b/w inline elements

--- js/form.js
+++ js/form.js
@@ -5,17 +5,22 @@ function addField()
   let newfield = document.getElementById('i1').cloneNode(true);
   itemCount++;
   newfield.id = 'i' + itemCount;
+  newfield.value = '';
 
   let db = document.getElementById('db1').cloneNode(true);
   db.id = 'db' + itemCount;
 
   let br = document.createElement("br");
+  let sp = document.createElement("span");
+  sp.innerHTML = '&nbsp';
 
   let addBtn = document.getElementById('addBtn');
   document.getElementById('itemform').appendChild(newfield);
+  document.getElementById('itemform').appendChild(sp);
   document.getElementById('itemform').appendChild(db);
   document.getElementById('itemform').appendChild(br);
   document.getElementById('itemform').appendChild(addBtn);
+  newfield.focus();
 }
 
 function readFields()
@@ -37,10 +42,13 @@ function delField(el)
   let id = el.id;
   let tid = 'i' + id.slice(2,);
   let tinput = document.getElementById(tid);
+  let sp = document.getElementById(tid).nextSibling;
   let br = document.getElementById(id).nextSibling;
+
   if (itemCount > 2 && id != 'db1')
   {
     document.getElementById('itemform').removeChild(el);
+    document.getElementById('itemform').removeChild(sp);
     document.getElementById('itemform').removeChild(tinput);
     document.getElementById('itemform').removeChild(br);
   }
